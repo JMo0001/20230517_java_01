4df17294087a772a861bc070a5b282bda0a6e0a1
JM00001 <liser1@naver.com>
2023-08-23 17:49:41 +0900

    오후 수업
    spring jdbc 연동

--- 20230823_spring_db/src/main/java/kh/lclass/db1/HomeController.java
+++ 20230823_spring_db/src/main/java/kh/lclass/db1/HomeController.java
@@ -1,4 +1,4 @@
-package kh.lclass.spring1;
+package kh.lclass.db1;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -37,12 +35,5 @@ public class HomeController {
 		
 		return "home";
 	}
-	
-	@GetMapping("/aaa/a1")
-	public void a1() {
-		
-	}
-	
-	
 	
 }
